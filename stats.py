69e00c34477db3625b551446da55331a15b94d15
davidb1101 <davidb1101@gmail.com>
2025-08-06 21:39:11 -0500

    half way through project

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    char_count_dict = {}
+    for char in text:
+        char = char.lower()
+        if char not in char_count_dict:
+            char_count_dict[char] = 1
+        else:
+            char_count_dict[char] += 1
+
+    return char_count_dict
